d266493f0490e361e1d1bfa0b8e8a2d769da53b8
Jorgelino328 <jorgewilliam328@gmail.com>
2026-06-02 14:34:01 -0300

    feat: connect play button to prototype level

--- ui/main_menu/main_menu.gd
+++ ui/main_menu/main_menu.gd
@@ -6,3 +6,7 @@ func _ready():
 
 func _on_title_animator_animation_finished(anim_name):
 	$TitleAnimator.play("idle")
+
+
+func _on_play_pressed():
+	get_tree().change_scene_to_file("res://scenes/level_prototype.tscn")
