38074bc9f52290be755ade9175ef6842c7f9e1ba
Jorgelino328 <jorgewilliam328@gmail.com>
2026-06-06 15:04:19 -0300

    feat: acresenta efeitos sonoros e outras melhorias no menu principal

--- ui/main_menu/main_menu.gd
+++ ui/main_menu/main_menu.gd
@@ -9,4 +9,15 @@ func _on_title_animator_animation_finished(anim_name):
 
 
 func _on_play_pressed():
+	$SelectSfx.play()
+
+
+func _on_mouse_entered() -> void:
+	$HoverSfx.play()
+
+#AVISO: Fazer o botão de play depender da música pra trocar de nível não é uma boa prática!
+#Idealmente a música deveria tocar num controlador de cena externo para que o som persista
+#Essa solução é apenas para esse protótipo simples
+func _on_select_sfx_finished() -> void:
 	get_tree().change_scene_to_file("res://scenes/level_prototype.tscn")
+	
