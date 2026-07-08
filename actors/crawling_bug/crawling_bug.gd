577f08c3ed146fc62876458ff5ab59b8e843c22f
Jorgelino328 <jorgewilliam328@gmail.com>
2026-07-08 20:04:26 -0300

    fix: solve issues with the bug enemy movement and limit in blatform

--- actors/crawling_bug/crawling_bug.gd
+++ actors/crawling_bug/crawling_bug.gd
@@ -15,26 +15,29 @@ const id = 1
 var move := Vector2.RIGHT
 var turn_speed := 15.0
 var knockback := Vector2.ZERO
-
-
+var platform_velocity := Vector2.ZERO 
 
 func _physics_process(delta):
 	if(is_dead):
 		anim_state = State.DYING
 	else:
-		## Se move e checa colisões 
-		var col := move_and_collide((move * speed * delta) + knockback) 
+		var step_movement = (move * speed * delta) + knockback + (platform_velocity * delta)
+		var col := move_and_collide(step_movement) 
 		
+		platform_velocity = Vector2.ZERO
 	
 		## Se encontrar parede, gira 90 graus pra cima
 		if col and col.get_normal().rotated(PI / 2).dot(move) < 0.5:
 			move = col.get_normal().rotated(PI / 2)
+			
+			# Captura a velocidade da parede
+			platform_velocity = col.get_collider_velocity() 
 		else:
 			var pos := position
 			col = move_and_collide(move.rotated(PI / 2) * 15)
 			
-			## Se não houver colisão ao tentar se virar pra baixo, gira aos poucos
-			## até achar o chão
+			## Se não houver colisão ao tentar se virar pra baixo, gira aos poucos [cite: 2]
+			## até achar o chão [cite: 2]
 			if not col:
 				for i in 10:
 					position = pos
@@ -43,9 +46,12 @@ func _physics_process(delta):
 					
 					if col:
 						move = col.get_normal().rotated(PI / 2)
+						platform_velocity = col.get_collider_velocity() 
 						break
 			else:
 				move = col.get_normal().rotated(PI / 2)
+				platform_velocity = col.get_collider_velocity() 
+
 		rotation = lerp_angle(rotation, move.angle(), turn_speed * delta)
 	
 	animation_handler()
@@ -59,7 +65,6 @@ func animation_handler():
 			if c_bug_animator.current_animation != "death":
 				c_bug_animator.play("death")
 				death_sfx.play()
-
 
 func _on_c_bug_animator_animation_finished(anim_name: StringName) -> void:
 	if anim_name == "death":
--- objects/platforms/bug_platform/bug_area.gd
+++ objects/platforms/bug_platform/bug_area.gd
@@ -1,11 +1,27 @@
 extends Area2D
 
-@export var bug_limit = 10
+@export var bug_limit = 3
+var signal_sent = false
 
 signal limit_reached()
 
-func _process(delta: float) -> void:
+
+func _physics_process(_delta) -> void:
 	var bodies = get_overlapping_bodies()
-	if bodies.size() >= bug_limit:
+	var alive_bugs = []
+	for body in bodies:
+		if body is Enemy and body.hp > 0: 
+			alive_bugs.append(body)
+	if alive_bugs.size() >= bug_limit and not signal_sent:
 		limit_reached.emit()
+		signal_sent = true
+	elif alive_bugs.size() > bug_limit:
+		var excess_amount = alive_bugs.size() - bug_limit
+		alive_bugs.shuffle()
+		for i in excess_amount:
+			var doomed_bug = alive_bugs[i]
+			doomed_bug.take_damage(doomed_bug.hp)
 	
+func _on_body_entered(body: Node2D) -> void:
+	if body is Enemy and body not in get_children():
+		body.call_deferred("reparent", self)
